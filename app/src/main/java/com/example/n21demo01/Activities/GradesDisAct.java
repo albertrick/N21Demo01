91d007600c4ed62fe8ef1654059c7aa332003761
albertrick <albertrick1010@gmail.com>
2025-03-15 18:55:06 +0200

    Grades display design fixed and programed

--- app/src/main/java/com/example/n21demo01/Activities/GradesDisAct.java
+++ app/src/main/java/com/example/n21demo01/Activities/GradesDisAct.java
@@ -2,6 +2,11 @@ package com.example.n21demo01.Activities;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,12 +16,49 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.n21demo01.R;
 
-public class GradesDisAct extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class GradesDisAct extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner spinStu, spinFocus;
+    private Switch swOrder;
+    private ListView lVGrades;
+    private String stuID;
+    private ArrayAdapter<String> adpStu, adpFiled;
+    private ArrayList<String> stuList, stuIDs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grades_dis);
+
+        spinStu = findViewById(R.id.spinStu);
+        spinFocus = findViewById(R.id.spinFocus);
+        swOrder = findViewById(R.id.swOrder);
+        lVGrades = findViewById(R.id.lVGrades);
+
+        stuID = "";
+        spinStu.setOnItemSelectedListener(this);
+        stuIDs = new ArrayList<String>();
+        stuList = new ArrayList<String>();
+        stuList.add("Choose a student:");
+        adpStu = new ArrayAdapter<String>(GradesDisAct.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, stuList);
+        spinStu.setAdapter(adpStu);
+
+        spinFocus.setOnItemSelectedListener(this);
+        String[] attributes = {"Choose filed to show by:", "Subject", "Grade type", "Grade"};
+        adpFiled = new ArrayAdapter<String>(GradesDisAct.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, attributes);
+        spinFocus.setAdapter(adpFiled);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 
     public void back(View view) {
